7acff2deaa69ed1c2d95b10fb5ff54c75f645f17
AdrianMonenciu <adrian.monenciu@gmail.com>
2023-07-20 21:47:59 +0100

    Update 2 2023.07.20

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
+const path = require('path');
 
 const app = require('express')();
-//const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -14,7 +14,8 @@ const indexRouter = require('../routes/index')
 const pasteBinRouter = require('../routes/pasteBin') 
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+//app.set('views', __dirname + '/views')
+app.set('views', path.join(__dirname, '/views'));
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(methodOverride('_method'))
@@ -29,8 +30,5 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/pastebin', pasteBinRouter)
-
-//app.listen(process.env.PORT || 3000)
-
 
 module.exports = app;
